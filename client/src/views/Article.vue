0497b4f33aa0fbce997b027670ec351e81d91687
sergeantO <morgueheadman@gmail.com>
2020-12-17 00:27:02 +0300

    Добавлен выделитель для заметок

--- client/src/views/Article.vue
+++ client/src/views/Article.vue
@@ -15,16 +15,18 @@
     </v-row>
     
     <v-row>
-      <v-col cols='6' offset="2">
+      <v-col cols='6' offset="2"  @mousedown='getPoint' @mouseup='mouseup'>
         <Editor v-if="editorData" :readMode="true" :initData="editorData" />
       </v-col>
     </v-row>
+    <div class="tools" ref='tool'>
+      <v-btn @click='add'>+</v-btn>
+    </div>
   </v-container>
-  
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Ref, Vue } from 'vue-property-decorator';
 // import { namespace } from 'vuex-class';
 // const App = namespace('App');
 
@@ -38,16 +40,73 @@ import ArticleService from '../services/ArticleService';
 export default class Article extends Vue {
   public $route: any; // bugfix
 
+  @Ref()
+  private readonly tool!: HTMLElement
+
   private editorData: object | null = null
   private title: string = ''
-  private image!: string
+  private image: string = 'https://cdn.pixabay.com/photo/2020/07/12/07/47/bee-5396362_1280.jpg'
+  private tracker = { x: 0, y: 0 }
 
   private created() {
-    ArticleService.getOne(this.id).then((data) => {
+    ArticleService.getOne(this.id)
+    .then((data) => {
       this.editorData = data.editorData
       this.title = data.title
       this.image = data.image || 'https://cdn.pixabay.com/photo/2020/07/12/07/47/bee-5396362_1280.jpg'
     })
+  }
+
+  private getPoint(e) {
+    this.tracker.x = e.pageX
+    this.tracker.y = e.pageY
+  }
+
+  private mouseup(e) {
+      let selectedText = this.getSelected()
+
+      if (e.pageY <= this.tracker.y) {
+        this.tracker.y = e.pageY
+        this.tracker.x = e.pageX
+      }
+
+      if(selectedText){
+        let left = this.tracker.x + 5
+        let top = this.tracker.y - 160
+        this.showBtn(left, top)
+      } else {
+        this.hideBtn()
+      }
+  }
+
+  private showBtn(left: number, top: number) {
+    if (!this.tool) return
+
+    let style = this.tool.style
+    style.display = 'block'
+    style.top = top + 'px'
+    style.left = left + 'px'
+  }
+
+  private hideBtn() {
+    if (!this.tool) return
+
+    let style = this.tool.style
+    style.display = 'none'
+  }
+
+  private getSelected(): string {
+      let t = null
+      if (window.getSelection) {
+        t = window.getSelection()
+      } else if (document.getSelection) {
+        t = document.getSelection()
+      }
+      return t!.toString()
+  }
+
+  private add() {
+    console.log( this.getSelected() )
   }
 
   private get id() {
@@ -56,3 +115,13 @@ export default class Article extends Vue {
 
 }
 </script>
+
+<style scoped>
+.tools {
+  display: none;
+  position: absolute;
+  background: #777;
+  z-index: 1;
+}
+
+</style>
