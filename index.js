4b2937db820ac2645a6e2c37f9940cbbba3b51d0
sergeantO <sergeantO>
2020-12-19 13:10:51 +0300

    server index refactoring

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require("express")
+const bodyParser = require("body-parser")
 const cors = require('cors')
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 const config = require("config")
+const path = require('path')
 
-const app = express();
+const app = express()
 
 app.use(cors())
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.use('/images', express.static(__dirname + '/images'));
+app.use('/images', express.static(__dirname + '/images'))
 
-app.use("/api/user", require("./routes/user"));
-app.use("/api/upload", require("./routes/upload"));
-app.use("/api/notes", require("./routes/notes"));
-app.use("/api/article", require("./routes/articles"));
+app.use("/api/user", require("./routes/user"))
+app.use("/api/upload", require("./routes/upload"))
+app.use("/api/notes", require("./routes/notes"))
+app.use("/api/article", require("./routes/articles"))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'dist')))
+
+  app.get('*', (req, res) => {
+    res.sendFile( path.join(__dirname, 'client', 'dist', 'index.html') )
+  })
+}
 
 // Erorrs
 app.use((req, res, next) => {
-  const error = new Error('Path not found');
+  const error = new Error('Path not found')
   error.status = 404
   next(error)
 })
@@ -34,7 +43,7 @@ app.use((error, req, res, next) => {
 })
 
 
-const PORT = process.env.PORT || config.get('port');
+const PORT = process.env.PORT || config.get('port')
 const start = async () => {
   try {
     await mongoose.connect(config.get('mongoUri'), {
@@ -45,14 +54,12 @@ const start = async () => {
     console.log("Connected to DB !!");
 
     app.listen(PORT, (req, res) => {
-      console.log(`Server Started at PORT ${PORT}`);
+      console.log(`Server Started at PORT ${PORT}`)
     });
   } catch (e) {
     console.log(e);
     process.exit(1)
   }
 };
-
-
 
 start()
